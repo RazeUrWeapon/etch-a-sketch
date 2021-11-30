d8942bf367296a7d5fd7299758f41ac116b4792e
Joseph <jwilde19@gmail.com>
2021-11-30 05:23:48 -0500

    Add game board, styling, and clear/reset button

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-// Create 256 (16x16) div elements for game board
-function gameBoard() {
-    const createSquares = document.getElementById('gameboard');
+const clearBtn = document.querySelector('button');
+const gameBoard = document.getElementById('gameboard');
 
-    for (i = 0; i < 256; i++) {
-        createSquares.innerHTML += '<div class="square"></div>';
+let size = 18 
+
+// Create 256 (16x16) div elements for first game board
+function generateBoard() {
+    gameBoard.style.gridTemplateColumns = `repeat(${size}, ${size}fr)`;
+    gameBoard.style.gridTemplateRows = `repeat(${size}, ${size}fr)`;
+
+    for (i = 0; i < size * size; i++) {
+        const newElement = document.createElement('div');
+        newElement.addEventListener('mouseover', hover);
+        gameBoard.appendChild(newElement);
     }
 }
 
-gameBoard()
+function hover(e) {
+    e.target.style.backgroundColor = 'darkgray';
+}
+
+generateBoard();
+
+
+
+clearBtn.addEventListener('click', () => {
+    gameBoard.innerHTML = '';
+    size = prompt('Pick your board size:');
+    if (size > 100) {
+        size = prompt('Please pick a number below 100');
+    } else {
+        generateBoard();
+    }
+});
